b7b4b18449b5adffd66c773f49532299c558f9d0
Amman Rizwan <97797756+AmmanRizwan@users.noreply.github.com>
2025-10-17 13:39:44 +0530

    Refactor main application to remove MainWindow and implement a frameless top window with a label

--- main.cpp
+++ main.cpp
@@ -1,11 +1,26 @@
-#include "mainwindow.h"
-
 #include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QVBoxLayout>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+
+    QWidget window;
+    window.setWindowFlags(Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint);
+    window.setAttribute(Qt::WA_TranslucentBackground);
+    window.setStyleSheet("background-color: rgba(30,30,30,220); border-radius: 10px");
+
+    QLabel *label = new QLabel("Always on Top Window App");
+    label->setStyleSheet("color: white; font-size:16px");
+
+    QVBoxLayout *layout = new QVBoxLayout(&window);
+    layout->addWidget(label);
+    layout->setContentsMargins(20,20,20,20);
+
+    window.setLayout(layout);
+    window.resize(300, 100);
+    window.show();
     return a.exec();
 }
